0d895bcb4b94336845fdf80cc7bab61d15031f8f
aadennis <dennis.circ@gmail.com>
2025-12-23 00:03:55 +0000

    tidy only

--- Grids/CodeGen/draw_lines.py
+++ Grids/CodeGen/draw_lines.py
@@ -1,22 +1,20 @@
 # For tab_devn.svg, generate 5 sets of 6 lines of tab.
 # x1 and x2 are always static, being the start and end of lines
 # After generation, paste into tab_dev03.svg.
-rows = 7 # 6 lines plus 1 spacer row
+rows = 7      # 6 strings + 1 spacer
 sets = 5
-x = 20
-width = 60
-height = 40
 y_start = 10
-y_step = 5 # increment between rows
+y_step = 5
 
-print(f'<!-- codegen inject start -->')
-for i in range(sets * rows):
-    y1 = y_start + i * y_step
-    # text_y = y + height // 2 + 2  # center vertically; tweak offset if needed
-    if i%rows == 0 and i != 0:
-        # print(f"{i}")
-        print("")
-        continue
-    print(f' <line class="std-line" x1="10" y1="{y1}" x2="370" y2="{y1}"/>')
-print(f'<!-- codegen inject end -->')    
+print('<!-- codegen inject start -->')
 
+for s in range(sets):
+    for r in range(rows):
+        if r == 0:
+            print("")   # spacer between staves
+            continue
+
+        y = y_start + (s * rows + r) * y_step
+        print(f' <line class="std-line" x1="10" y1="{y}" x2="370" y2="{y}"/>')
+
+print('<!-- codegen inject end -->')
